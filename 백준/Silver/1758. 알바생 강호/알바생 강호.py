caedb210f2fb41490fcadcd03908b17ab13b4df9
Jaeeun <128143640+je326@users.noreply.github.com>
2024-01-09 23:33:22 +0900

    [Silver IV] Title: 알바생 강호, Time: 3760 ms, Memory: 34972 KB -BaekjoonHub

---
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.py
@@ -0,0 +1,17 @@
+import sys
+read = sys.stdin.readline
+
+N = int(read())
+tips = [int(input()) for _ in range(N)]
+
+tips.sort(reverse=True)
+
+sum = 0
+index = 0
+
+for t in tips:
+    sum += max(t - tips.index(t, index, len(tips)), 0)
+    index += 1
+
+print(sum)
+
