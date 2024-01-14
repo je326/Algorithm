c4f0e7e7221c02698e6dbd9ce803e317e78bdf46
Jaeeun <128143640+je326@users.noreply.github.com>
2024-01-14 22:42:23 +0900

    [Gold V] Title: 적록색약, Time: 72 ms, Memory: 31564 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,41 +1,37 @@
-from collections import deque
 import sys
 read = sys.stdin.readline
+sys.setrecursionlimit(100000)
 
 N = int(read())
 graph = [list(read()) for _ in range(N)]
-Q = deque([])
 
 #상하좌우
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bfs(x,y):
-    Q.append([x,y])
+def dfs(x,y):
     visited[x][y] = True
-    while Q:
-        x,y = Q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < N and graph[nx][ny] == graph[x][y] and visited[nx][ny] == False:
-                Q.append([nx,ny])
-                visited[nx][ny] = True
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < N and 0 <= ny < N and graph[nx][ny] == graph[x][y] and visited[nx][ny] == False:
+            dfs(nx,ny)
 
-#적록색약이 아닌 경우
-visited = [[False]*N for _ in range(N)]
+
+# 적록색약이 아닌 경우
+visited = [[False] * N for _ in range(N)]
 ans1 = 0
 for i in range(N):
     for j in range(N):
         if not visited[i][j]:
-            bfs(i,j)
+            dfs(i, j)
             ans1 += 1
 
-#적록색약인 경우
-visited = [[False]*N for _ in range(N)]
+# 적록색약인 경우
+visited = [[False] * N for _ in range(N)]
 ans2 = 0
 
-#G와 R 같은 색상으로 인식
+#G와 R 같은 색상으로 인식 
 for i in range(N):
     for j in range(N):
         if graph[i][j] == 'G':
@@ -44,7 +40,7 @@ for i in range(N):
 for i in range(N):
     for j in range(N):
         if not visited[i][j]:
-            bfs(i,j)
+            dfs(i,j)
             ans2 += 1
 
 print(ans1, ans2)
