3442e93168f5bc1195033ecf95bfdcca97929536
Jaeeun <128143640+je326@users.noreply.github.com>
2024-03-28 14:15:46 +0900

    [level 2] Title: 할인 행사, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,16 +1,28 @@
+from collections import defaultdict
+
 def solution(want, number, discount):
     answer = 0
+    want_dict = dict(zip(want, number))
     
-    for i in range(0, len(discount) - 9):
-        discount_10 = discount[i: i+10]
-        buy = True
+    discount_dict = defaultdict(int)
+    
+    for d in range(10):
+        discount_dict[discount[d]] += 1
+        answer += check(want_dict, discount_dict)
         
-        for j in range(len(want)):
-            if discount_10.count(want[j]) != number[j]:
-                buy = False
-                break
-                
-        if buy:
-            answer += 1
-                 
+    for i in range(len(discount) - 10):
+        if discount_dict[discount[i]] < 1:
+            discount_dict.pop(discount[i])
+        else:
+            discount_dict[discount[i]] -= 1
+        
+        discount_dict[discount[i + 10]] += 1
+        
+        answer += check(want_dict, discount_dict)
+        
     return answer
+        
+def check(want_dict, discount_dict):
+    return all(want_dict[item] == discount_dict[item] for item in want_dict)
+    
+    
