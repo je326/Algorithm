b86ca0d65ca3231a39b7ace8513b64e62baf2862
Jaeeun <128143640+je326@users.noreply.github.com>
2024-03-28 14:23:27 +0900

    [level 2] Title: 할인 행사, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -11,11 +11,7 @@ def solution(want, number, discount):
         answer += check(want_dict, discount_dict)
         
     for i in range(len(discount) - 10):
-        if discount_dict[discount[i]] < 1:
-            discount_dict.pop(discount[i])
-        else:
-            discount_dict[discount[i]] -= 1
-        
+        discount_dict[discount[i]] -= 1
         discount_dict[discount[i + 10]] += 1
         
         answer += check(want_dict, discount_dict)
