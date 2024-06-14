9dd8d4b31b9dfee2861311d00d37977d242a3990
Jaeeun <128143640+je326@users.noreply.github.com>
2024-06-14 15:26:28 +0900

    [Gold IV] Title: 팀 빌딩, Time: 132 ms, Memory: 42204 KB -BaekjoonHub

--- 백준/Gold/22945. 팀 빌딩/팀 빌딩.py
+++ 백준/Gold/22945. 팀 빌딩/팀 빌딩.py
@@ -2,17 +2,17 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-arr = list(map(int, input().split()))
+users = list(map(int, input().split()))
+abilities = []
 
-max_status = 0
 start, end = 0, N-1
 while start < end:
-    status = (end - start - 1) * min(arr[start], arr[end])
-    max_status = max(max_status, status)
-    if arr[start] <= arr[end]:
+    min_ability = min(users[start], users[end])
+    abilities.append(min_ability * (end - start - 1))
+
+    if users[start] < users[end]:
         start += 1
     else:
         end -= 1
 
-
-print(max_status)
+print(max(abilities))
