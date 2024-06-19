54f71a03fba2e7d646084d356d9da65ec71561f1
Jaeeun <128143640+je326@users.noreply.github.com>
2024-06-19 17:09:30 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -9,39 +9,29 @@ def calculate(temp, num, op):
     elif op == 2:
         return temp * num
     elif op == 3:
-        if temp < 0:
-            return -(-temp // num)
-        else:
-            return temp // num
+        return int(temp / num)
 
 
-def dfs(temp, num_idx, operators_visited):
+
+def dfs(temp, num_idx, operators_cnt):
     if num_idx == N-1:
         results.append(temp)
         return
 
-    for i in range(len(operators)):
-        if not operators_visited[i]:
-            operators_visited[i] = True
-            new_temp = calculate(temp, nums[num_idx + 1], operators[i])
-            dfs(new_temp, num_idx + 1, operators_visited)
-            operators_visited[i] = False
-
+    for i in range(4):
+        if operators_cnt[i] > 0:
+            operators_cnt[i] -= 1
+            new_temp = calculate(temp, nums[num_idx + 1], i)
+            dfs(new_temp, num_idx+1, operators_cnt)
+            operators_cnt[i] += 1
 
 N = int(input())
 nums = list(map(int, input().split()))
 operators_cnt = list(map(int, input().split()))
 
-#주어진 연산자들을 0, 1, 2, 3으로 +, -, *, //을 표현하여 operators 리스트로 관리
-operators = []
-for idx, cnt in enumerate(operators_cnt):
-    for c in range(cnt):
-        operators.append(idx)
-
-operators_visited = [False] * len(operators)
 results = []
 
-dfs(nums[0], 0, operators_visited)
+dfs(nums[0], 0, operators_cnt)
 
 print(max(results))
 print(min(results))
