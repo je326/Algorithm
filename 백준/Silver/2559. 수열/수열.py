1104861a958b7e43eb4f3aa4590059933d136728
Jaeeun <128143640+je326@users.noreply.github.com>
2024-06-27 19:57:49 +0900

    [Silver III] Title: 수열, Time: 96 ms, Memory: 40156 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -4,20 +4,11 @@ input = sys.stdin.readline
 N, K = map(int, input().split())
 temperatures = list(map(int, input().split()))
 
-start, end = 0, K-1
-temp = sum(temperatures[start:end+1])
+temp = sum(temperatures[:K])
 ans = temp
 
-start += 1
-end += 1
-
-while end < N:
-    temp -= temperatures[start-1]
-    temp += temperatures[end]
-
-    start += 1
-    end += 1
-
+for idx in range(1, N-K+1):
+    temp = temp - temperatures[idx-1] + temperatures[idx+K-1]
     ans = max(ans, temp)
 
 print(ans)
